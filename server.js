2409882c1080d72906a9c8c2d110332a0a78aec7
labor-day <shaffer.scott@yahoo.com>
2022-07-27 12:22:01 -0600

    server running

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 // Your code here
+const responseBody = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h1>Hello there!</h1>
+  </body>
+  </html>
+`;
+
+let http = require('http');
+
+let serverFunction = (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(responseBody);
+}
+
+const server = http.createServer(serverFunction);
+
+const port = 5000;
+
+const logFunction = () => {
+  console.log('Server is listening on port', port);
+}
+
+server.listen(port, logFunction);
